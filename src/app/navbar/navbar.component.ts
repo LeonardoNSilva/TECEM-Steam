3dc0b43789f7f19803bc2988229a05e453620c89
Leonardo do Nascimento <nascimento_leonardo@educadventista.org>
2021-11-27 00:48:36 -0300

    Finalização 1.0

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  rotas: any = [
+    {rota: 'acao'},
+    {rota: 'aventura'},
+    {rota: 'casual'},
+    {rota: 'competitivo-online'},
+    {rota: 'corrida'},
+    {rota: 'esporte'},
+    {rota: 'estrategia'},
+    {rota: 'multijogador'},
+    {rota: 'rpg'},
+    {rota: 'simulador'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
